272a01c14a82bd0ba4be2193533ded619cc937ad
Steven Smith <s_smith1998@outlook.com>
2021-11-20 22:43:34 -0500

    Add new RequestTypes

--- crunchyroll_connect/utils/types.py
+++ crunchyroll_connect/utils/types.py
@@ -1,6 +1,12 @@
 from enum import Enum
 
+
 class RequestType(Enum):
-    LOGIN = "login"
-    LOGOUT = "logout"
+    LOGIN = 'login'
+    LOGOUT = 'logout'
     CREATE_SESSION = 'start_session'
+    LIST_LOCALES = 'list_locales'
+    LIST_MEDIA = 'list_media'
+    LIST_COLLECTION = 'list_collections'
+    INFO = 'info'
+    CATEGORIES = 'categories'
